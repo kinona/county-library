1ba1865aa4a5dc9a77e30b0de80e5cf8236850cb
kinona <97958646+kinona@users.noreply.github.com>
2022-02-08 17:20:02 +0300

    Add files via upload

---
+++ library system.c
@@ -0,0 +1,30 @@
+/*
+county library Management System
+by Alice Kinona
+Feb 2022
+MIT License
+C89 Compiler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int menu(){
+  int action;
+printf("Select an action:\n");
+printf("1.add a new patron\n");
+printf("2.View patron\n");
+printf("3.View books\n");
+printf("4.add new book\n");
+printf("your action:\n");
+scanf["%d",&action];
+return action;
+}
+
+int main()
+{
+    printf("county library system!\n");
+    printf("Welcome ms. Kinona !\n");
+    printf{"you selected action %d",menu());
+    return 0;
+
+}
